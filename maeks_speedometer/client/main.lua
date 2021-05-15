19d4f60a16eedd2e3a503c61feb748d839725979
Maeks <84164511+5Maeks@users.noreply.github.com>
2021-05-15 03:21:55 +0200

    Add files via upload

---
+++ maeks_speedometer/client/main.lua
@@ -0,0 +1,29 @@
+Citizen.CreateThread(function()
+    while true do
+        local ped = PlayerPedId()
+        local isInVehicle = IsPedInAnyVehicle(ped, false)
+
+        Citizen.Wait(Config['TickTime'])
+
+        if isInVehicle then
+
+            Citizen.Wait(0)
+
+            local vehicle = GetVehiclePedIsIn(ped, false)
+            local fuel = GetVehicleFuelLevel(vehicle)
+            local gear = GetVehicleCurrentGear(vehicle)
+            local speed = GetEntitySpeed(vehicle)*3.6 -- KMH
+
+            SendNUIMessage({
+                isInVehicle = isInVehicle;
+                speed = speed;
+                fuel = fuel;
+                gear = gear;
+            });
+        else
+            SendNUIMessage({
+                isInVehicle = false;
+            });
+        end
+    end
+end)
